d2f2fe96916cf18bf18247a33e14d5226c417aab
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-04 23:06:17 +0300

    Registered the User model in the admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'is_staff')
+    list_filter = ('date_joined', 'groups', 'is_staff', 'is_superuser','is_active')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+
+    fieldsets = (
+		(_('Credentials'), {'fields': ('username', 'password')}),
+		(_('Personal info'), {
+			'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+		(_('Others'), {'fields': ('bio', 'profile_picture',)}),
+		(_('Permissions'), {
+			'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+		}),
+		(_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+	)
+
+    add_fieldsets = (
+        (None, {
+	        'classes': ('wide',),
+	        'fields': ('username', 'email', 'phone_number', 'password1', 'password2'),
+        }),
+    )
+
