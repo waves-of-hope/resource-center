cb806f4007889902705df48bae0b6feae9649c42
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-09-08 17:05:14 +0300

    Moved generic templates to base directory

--- resource_center/settings.py
+++ resource_center/settings.py
@@ -70,7 +70,9 @@ ROOT_URLCONF = 'resource_center.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
