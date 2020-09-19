469fdd1b85c55abe14bcf16a9fbd5e0989436a6a
Harison Mwangi <harisongachuru@gmail.com>
2020-09-19 21:35:07 +0300

    Refactored resource.models tests

--- resources/tests/test_models.py
+++ resources/tests/test_models.py
@@ -4,53 +4,186 @@ from django.utils import timezone
 
 from resources.models import Category, Tag, Book
 
-class BookModelTestCase(TestCase):
+class CategoryModelTestCase(TestCase):
+    """
+    Tests for the Category model
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-    def setUp(self):
-        self.category = Category.objects.create(
+        cls.category = Category.objects.create(
+            name='Spiritual',
+            description='Materials that can help you grow '
+                'Spiritually',
+            slug='spiritual'
+        )
+    
+    def test_category_basic(self):
+        """
+        Test the basic functionality of Category
+        """
+        self.assertEqual(self.category.name, 'Spiritual')
+        self.assertEqual(self.category.description,
+            'Materials that can help you grow Spiritually')
+
+    def test_category_object_name(self):
+        """
+        Test the name of the Category object that will
+        be shown in django admin
+        """
+        self.assertEqual(self.category.name, str(self.category))
+
+    def test_name_meta(self):
+        """
+        Test meta attributes of the name field
+        """
+        name__meta = self.category._meta.get_field('name')
+        self.assertEqual(name__meta.verbose_name, 'name')
+        self.assertEqual(name__meta.max_length, 30)
+        self.assertEqual(name__meta.null, False)
+        self.assertEqual(name__meta.blank, False)
+
+    def test_description_meta(self):
+        """
+        Test meta attributes of the description field
+        """
+        description__meta = self.category._meta.get_field('description')
+        self.assertEqual(description__meta.verbose_name, 'description')
+        self.assertEqual(description__meta.max_length, 100)
+        self.assertEqual(description__meta.null, True)
+        self.assertEqual(description__meta.blank, True)
+
+    def test_slug_meta(self):
+        """
+        Test meta attributes of the slug field
+        """
+        slug__meta = self.category._meta.get_field('slug')
+        self.assertEqual(slug__meta.verbose_name, 'slug')
+        self.assertEqual(slug__meta.max_length, 30)
+        self.assertEqual(slug__meta.null, False)
+        self.assertEqual(slug__meta.blank, False)
+
+
+class TagModelTestCase(TestCase):
+    """
+    Tests for the Tag model
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.tag = Tag.objects.create(
+            name='Getting born again',
+            description='Addresses why and how to accept God\'s '
+                'free gift of salvation',
+            slug='getting-born-again'
+        )
+    
+    def test_tag_basic(self):
+        """
+        Test the basic functionality of Tag
+        """
+        self.assertEqual(self.tag.name, 'Getting born again')
+        self.assertEqual(self.tag.description,
+            'Addresses why and how to accept God\'s '
+                'free gift of salvation')
+        
+    def test_tag_object_name(self):
+        """
+        Test the name of the Tag object that will
+        be shown in django admin
+        """
+        self.assertEqual(self.tag.name, str(self.tag))
+
+    def test_name_meta(self):
+        """
+        Test meta attributes of the name field
+        """
+        name__meta = self.tag._meta.get_field('name')
+        self.assertEqual(name__meta.verbose_name, 'name')
+        self.assertEqual(name__meta.max_length, 30)
+        self.assertEqual(name__meta.null, False)
+        self.assertEqual(name__meta.blank, False)
+
+    def test_description_meta(self):
+        """
+        Test meta attributes of the description field
+        """
+        description__meta = self.tag._meta.get_field('description')
+        self.assertEqual(description__meta.verbose_name, 'description')
+        self.assertEqual(description__meta.max_length, 100)
+        self.assertEqual(description__meta.null, True)
+        self.assertEqual(description__meta.blank, True)
+
+    def test_slug_meta(self):
+        """
+        Test meta attributes of the slug field
+        """
+        slug__meta = self.tag._meta.get_field('slug')
+        self.assertEqual(slug__meta.verbose_name, 'slug')
+        self.assertEqual(slug__meta.max_length, 30)
+        self.assertEqual(slug__meta.null, False)
+        self.assertEqual(slug__meta.blank, False)
+
+
+class ResourceModelsTestCase(TestCase):
+    """
+    Sets up data to be shared across Resource Model tests
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.category = Category.objects.create(
             name='Finance',
             slug='finance'
         )
 
-        self.tag1 = Tag.objects.create(
+        cls.tag1 = Tag.objects.create(
             name='Christian finance',
             description='Explores the topic of finance from '
                 'a Christian perspective',
             slug='christian-finance'
         )
 
-        self.tag2 = Tag.objects.create(
+        cls.tag2 = Tag.objects.create(
             name='Wealth creation',
             slug='wealth-creation'
         )
 
-        self.User = get_user_model()
+        cls.User = get_user_model()
         
-        self.admin_user = self.User.objects.create_superuser(
+        cls.admin_user = cls.User.objects.create_superuser(
             first_name = 'Kelvin',
             email='kelvin@murage.com',
             password='kelvinpassword'
         )
 
-        self.user = self.User.objects.create_user(
+        cls.user = cls.User.objects.create_user(
             first_name = 'Alvin',
             last_name = 'Mukuna',
             email = 'alvin@mukuna.com',
             phone_number = '+254 701 234 567',
             password = 'alvinpassword'
         )
-            
-        self.book = Book.objects.create(
+
+        cls.book = Book.objects.create(
             title='A Christian\'s guide to wealth creation',
             summary='A step-by-step guide on how to handle money'
                 'as God has instructed in His Word',
-            category=self.category,
+            category=cls.category,
             slug='a-christians-guide-to-wealth-creation'
         )        
         # Direct assignment of many-to-many types not allowed.
-        self.book.authors.add(self.admin_user, self.user)
-        self.book.tags.add(self.tag1, self.tag2)
+        cls.book.authors.add(cls.admin_user, cls.user)
+        cls.book.tags.add(cls.tag1, cls.tag2)
+    
 
+class BookModelTestCase(ResourceModelsTestCase):
+    """
+    Tests for the Book model
+    """
     def test_book_basic(self):
         """
         Test the basic functionality of Book
@@ -190,116 +323,3 @@ class BookModelTestCase(TestCase):
         self.assertEqual(last_edit__meta.verbose_name, 'last edit')
         self.assertEqual(last_edit__meta.null, True)
         self.assertEqual(last_edit__meta.blank, True)
-
-
-class CategoryModelTestCase(TestCase):
-
-    def setUp(self):        
-        self.category = Category.objects.create(
-            name='Spiritual',
-            description='Materials that can help you grow '
-                'Spiritually',
-            slug='spiritual'
-        )
-
-    def test_category_basic(self):
-        """
-        Test the basic functionality of Category
-        """
-        self.assertEqual(self.category.name, 'Spiritual')
-        self.assertEqual(self.category.description,
-            'Materials that can help you grow Spiritually')
-
-    def test_category_object_name(self):
-        """
-        Test the name of the Category object that will
-        be shown in django admin
-        """
-        self.assertEqual(self.category.name, str(self.category))
-
-    def test_name_meta(self):
-        """
-        Test meta attributes of the name field
-        """
-        name__meta = self.category._meta.get_field('name')
-        self.assertEqual(name__meta.verbose_name, 'name')
-        self.assertEqual(name__meta.max_length, 30)
-        self.assertEqual(name__meta.null, False)
-        self.assertEqual(name__meta.blank, False)
-
-    def test_description_meta(self):
-        """
-        Test meta attributes of the description field
-        """
-        description__meta = self.category._meta.get_field('description')
-        self.assertEqual(description__meta.verbose_name, 'description')
-        self.assertEqual(description__meta.max_length, 100)
-        self.assertEqual(description__meta.null, True)
-        self.assertEqual(description__meta.blank, True)
-
-    def test_slug_meta(self):
-        """
-        Test meta attributes of the slug field
-        """
-        slug__meta = self.category._meta.get_field('slug')
-        self.assertEqual(slug__meta.verbose_name, 'slug')
-        self.assertEqual(slug__meta.max_length, 30)
-        self.assertEqual(slug__meta.null, False)
-        self.assertEqual(slug__meta.blank, False)
-
-
-class TagModelTestCase(TestCase):
-
-    def setUp(self):        
-        self.tag = Tag.objects.create(
-            name='Getting born again',
-            description='Addresses why and how to accept God\'s '
-                'free gift of salvation',
-            slug='getting-born-again'
-        )
-
-    def test_tag_basic(self):
-        """
-        Test the basic functionality of Tag
-        """
-        self.assertEqual(self.tag.name, 'Getting born again')
-        self.assertEqual(self.tag.description,
-            'Addresses why and how to accept God\'s '
-                'free gift of salvation')
-        
-    def test_tag_object_name(self):
-        """
-        Test the name of the Tag object that will
-        be shown in django admin
-        """
-        self.assertEqual(self.tag.name, str(self.tag))
-
-    def test_name_meta(self):
-        """
-        Test meta attributes of the name field
-        """
-        name__meta = self.tag._meta.get_field('name')
-        self.assertEqual(name__meta.verbose_name, 'name')
-        self.assertEqual(name__meta.max_length, 30)
-        self.assertEqual(name__meta.null, False)
-        self.assertEqual(name__meta.blank, False)
-
-    def test_description_meta(self):
-        """
-        Test meta attributes of the description field
-        """
-        description__meta = self.tag._meta.get_field('description')
-        self.assertEqual(description__meta.verbose_name, 'description')
-        self.assertEqual(description__meta.max_length, 100)
-        self.assertEqual(description__meta.null, True)
-        self.assertEqual(description__meta.blank, True)
-
-    def test_slug_meta(self):
-        """
-        Test meta attributes of the slug field
-        """
-        slug__meta = self.tag._meta.get_field('slug')
-        self.assertEqual(slug__meta.verbose_name, 'slug')
-        self.assertEqual(slug__meta.max_length, 30)
-        self.assertEqual(slug__meta.null, False)
-        self.assertEqual(slug__meta.blank, False)
