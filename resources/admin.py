eaaa4261e20a6882884fbfb55a0eb5697303cd94
Harison Mwangi <harisongachuru@gmail.com>
2020-09-24 14:20:33 +0300

    Added prepopulated fields and customised ordering of fields in add and change forms

--- resources/admin.py
+++ resources/admin.py
@@ -2,13 +2,73 @@ from django.contrib import admin
 
 from .models import Category, Tag, Book, Video
 
-admin.site.register(Category)
-admin.site.register(Tag)
+class ResourceGroupAdmin(admin.ModelAdmin):
+    """
+    Defines common admin site configurations for 
+    Resource Group models
+    """
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(ResourceGroupAdmin):
+    """
+    Registers the Category model to the admin site
+    """
+    fields = ('name', 'slug', 'description')
+
+
+@admin.register(Tag)
+class TagAdmin(ResourceGroupAdmin):
+    """
+    Registers the Tag model to the admin site
+    """
+    fields = ('name', 'slug')
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    """
+    Defines common admin site configurations for
+    Resource models
+    """    
+    list_display = ['title', 'category', 'display_tags']
+    prepopulated_fields = {'slug': ('title',)}
+
 
 @admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'display_tags']
+class BookAdmin(ResourceAdmin):
+    """
+    Registers the Book model to the admin site
+    """
+    fieldsets = (
+        ('Basic Info', {
+            'fields': ('title', 'slug', 'summary')
+        }),
+        ('External References', {
+            'fields': ('authors', 'category', 'tags')
+        }),
+        ('Uploads', {
+            'fields': ('cover_image', 'file_upload'),
+        }),
+        ('Additional Info', {
+            'fields': ('date_posted',)
+        })
+    )
+
 
 @admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'display_tags']
+class VideoAdmin(ResourceAdmin):
+    """
+    Registers the Video model to the admin site
+    """
+    fieldsets = (
+        ('Basic Info', {
+            'fields': ('title', 'slug', 'summary', 'url')
+        }),
+        ('External References', {
+            'fields': ('authors', 'category', 'tags')
+        }),
+        ('Additional Info', {
+            'fields': ('date_posted',)
+        })
+    )
