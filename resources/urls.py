b2acfccad230a96ed406230f546d3efba3cc6063
Harison Mwangi <harisongachuru@gmail.com>
2021-02-11 09:46:56 +0300

    Updated URLconf for books

--- resources/urls.py
+++ resources/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books'),
     path('videos/', views.VideoListView.as_view(), name='videos'),
-    path('b/<slug:slug>/', views.BookDetailView.as_view(), name='book'),
     path('v/<slug:slug>/', views.VideoDetailView.as_view(), name='video')
 ]
