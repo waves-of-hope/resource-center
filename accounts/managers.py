535f0097f12f601e2def91fb7147da67cc04163c
Harison Mwangi <harisongachuru@gmail.com>
2021-02-22 16:23:30 +0300

    Moved user manager tests to its module

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    """
-    A custom user model manager where an email is the unique
-    identifier for authentication instead of a username.
+    """A custom user model manager
+
+    Args:
+        BaseUserManager (object): Provides methods for creating a user
+        and superuser for a custom user model with fields not similar to
+        Django's default user
     """
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given first name,
-        last name, email and password.
+        """Creates a normal user account with the given email and
+        password plus any other attributes
+
+        Args:
+            email (str): An email address
+            password (str): A password
+
+        Raises:
+            ValueError: If an email address isn't provided
+
+        Returns:
+            object: An instance of the User model
         """
         if not email:
             raise ValueError('Email address must be set')
@@ -19,8 +31,18 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
+        """Creates a superuser account with the given email and password
+
+        Args:
+            email (str): An email address
+            password (str): A password
+
+        Raises:
+            ValueError: If the is_staff flag is not set to True
+            ValueError: If the is_superuser flag is not set to True
+
+        Returns:
+            object: An instance of the User model
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
