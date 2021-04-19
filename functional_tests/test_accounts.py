cc03dd2647b5bfed915f3c21cb6575fbaebb2d59
Harison Mwangi <harisongachuru@gmail.com>
2021-04-19 17:58:41 +0300

    Added explicit waits to failing tests

--- functional_tests/test_accounts.py
+++ functional_tests/test_accounts.py
@@ -150,10 +150,10 @@ class MemberTestCase(ResourceCenterTestCase):
         # TODO: fails in CI
         alerts = self.browser.find_elements_by_css_selector(
             '.alert')
-        self.assertIn(
+        utils.test.explicit_wait(self.assertIn(
             'Your profile has been updated\n×',
             [alert.text for alert in alerts]
-        )
+        ))
         updated_profile_form = self.browser.find_element_by_id('profile_form')
 
         self.assertEqual(
