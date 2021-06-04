aebe0c30fe4f0048e8671981925c80dc69394362
Harison Gachuru <harisongachuru@gmail.com>
2021-06-04 14:48:09 +0300

    Update context_processors.py

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,8 +2,7 @@ from django.conf import settings
 
 
 def google_analytics(request):
-    """Adds the Google Analytics ID to the context
-    """
+    """Adds the Google Analytics ID to the context"""
     return {
         'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID
     }
